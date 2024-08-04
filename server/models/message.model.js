21325d08fbea76c114140d80fe153c21ddede896
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-04 14:00:19 +0100

    conversation, message and user models

---
+++ server/models/message.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recieverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } // for createdAt and updatedAt fields - client - message.createdAt = 15:30
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
