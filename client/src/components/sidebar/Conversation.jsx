79119c5450b979b922d917faf5d35d4092501714
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-13 12:24:47 +0100

    get conversation functionality

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,26 +1,32 @@
-const Conversation = () => {
+import useConversation from "../../zustand/useConversation";
+
+const Conversation = ({ conversation, emoji, lastIndx }) => {
+  // this is a custom hook created using zustand, to create these global state values, alternative to useContext+provider etc...
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
-       
+      <div /* if conversation div is clicked/selected, div bg = blue */
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+        ${isSelected ? "bg-sky-500" : ""} 
+        `}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              alt="user avatar"
-            ></img>
+            <img src={conversation.profilePic} alt="user avatar"></img>
           </div>
         </div>
-       
+
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">John Doe</p>
-            <span className="text-xl">🔥</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
 
-      <div className="divider my-0 py-0 h-1"/>
+      {!lastIndx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,22 +1,26 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-    return (
-        <div className="py-2 flex flex-col overflow-auto"> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-            <Conversation/> 
-        </div>
-    )
-}
+  const { loading, conversations } = useGetConversations();
+  console.log("getUsersForSideBar:", conversations);
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation} // pass individual convo obj as a prop
+          emoji={getRandomEmoji()}
+          lastIndx={idx === conversations.length - 1}
+        />
+      ))}
 
-export default Conversations;   
+      {loading ? <span className="loading loading-spinner mx-auto" /> : null}
+    </div>
+  );
+};
+
+export default Conversations;
 
 /* overflow-auto: gives scroll bar for overflow */
