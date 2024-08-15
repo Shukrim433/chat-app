569f0e8fdd8be98e9ed810cbc3989de34962a4c5
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-15 12:57:49 +0100

    deployment

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +21,13 @@ app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist"))); // to serve the static files in the dist folder - __dirname is the root folder in this project
+
+app.get("*", (req, res) => {
+  // fallback
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 /* app.get("/", (req, res) => {
   // root route http://localhost:5000/
